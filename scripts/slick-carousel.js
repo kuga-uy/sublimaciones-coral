32d0751c844077ace04ecd5d5f822ef734b9ac91
Eduardo <eduardodiaz250196@gmail.com>
2021-04-02 15:58:21 -0300

    prototype page

---
+++ scripts/slick-carousel.js
@@ -0,0 +1,16 @@
+
+
+
+
+$('.slick-carousel').slick({
+    infinite: true,
+    centerMode: true,
+    responsive: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    arrows: true,
+
+  });        
