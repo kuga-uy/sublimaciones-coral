67605fafe8172c97b0c68af7416000b24819b74e
Eduardo <eduardodiaz250196@gmail.com>
2021-04-04 00:34:40 -0300

    making it responsive

--- scripts/slick-carousel.js
+++ scripts/slick-carousel.js
@@ -2,15 +2,31 @@
 
 
 
-$('.slick-carousel').slick({
-    infinite: true,
-    centerMode: true,
-    responsive: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    centerMode: true,
-    arrows: true,
+  if (screen.width >= 1360) {
+  $('.slick-carousel').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: true,
+      arrows: true,  
+    }); 
 
-  });        
+  } else if (screen.width < 1360) {
+    $('.slick-carousel').slick({
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: true,
+      arrows: true,
+      infinite: true,
+      centerMode: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: true,
+    });
+  }
